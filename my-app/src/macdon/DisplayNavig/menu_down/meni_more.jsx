d6e55f674411bc9bc696e683f65cfd8be87da386
VasiliiV <volgincoin@list.ru>
2022-12-01 09:58:50 +0300

    DEV_02 Add display navigation

---
+++ my-app/src/macdon/DisplayNavig/menu_down/meni_more.jsx
@@ -0,0 +1,11 @@
+import "./menu_down.css"
+
+function MenuMore() {
+    return (
+        <div className='menu-categories__more'>
+
+        </div>
+    )
+}
+
+export default MenuMore;
---
+++ my-app/src/macdon/DisplayNavig/menu_down/menu_wrap.jsx
@@ -0,0 +1,11 @@
+import "./menu_down.css"
+
+function MenuWrap() {
+    return (
+        <div className='menu-categories__wrap'>
+
+        </div>
+    )
+}
+
+export default MenuWrap;
